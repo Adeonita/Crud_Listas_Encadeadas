fd35e6d53bab82096e000ae01095ae94347f270d
Adeonita dos Santos <40674971+Adeonita@users.noreply.github.com>
2023-05-12 17:49:55 -0300

    Feat/adjusts (#4)
    * chore: add list file
    
    * chore: move file functions
    
    * chore: add struct file
    
    * chore: add helper file
    
    * chore: remove definitions
    
    * chore: remove helpers
    
    * chore: remove unecesary import
    
    * update configuration file

---
+++ list/list.h
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "./helpers/helper.h"
+#include "struct.h"
+
+#define DATABASE "./database.txt"
+
+List* create()
+{
+    List* list = (List*) malloc(sizeof(List));
+    list->first = NULL;
+    list->totalElements = 0;
+
+    return list;
+}
+
+void insert(List* list, Client client)
+{
+    ListNode* ant = NULL;
+    ListNode* first = list->first;
+    ListNode* node = (ListNode*) malloc(sizeof(ListNode));
+    
+
+    while (first != NULL)
+    {
+        ant = first;
+        first = first->next;
+    }
+    
+    client.active = 1;
+    client.id = list->totalElements;
+
+    node->client = client;
+
+    if (ant == NULL) {
+        node->next = list->first; //Seta o fim da lista, atribuindo null a última posição do ponteiro na primeira atribuição
+        list->first = node;
+    } else {
+        node->next = ant->next;
+        ant->next = node;
+    }
+    
+    list->totalElements ++;
+    
+    return;
+}
+
+void getAll(List* list)
+{
+    for (ListNode* ln = list->first; ln != NULL; ln = ln->next)
+    {
+        printf("Id: %d ", ln->client.id);
+        printf("Nome: %s ", ln->client.name);
+        printf("Idade: %d ", ln->client.age);
+        printf("\n");
+    }
+
+    return;
+}
+
+void get(List* list, int id)
+{   
+    for (ListNode* ln = list->first; ln != NULL; ln = ln->next)
+    {
+        if (ln->client.id == id) {
+            printf("\n\n");
+            printf("Id: %d ", ln->client.id);
+            printf("Nome: %s ", ln->client.name);
+            printf("Idade: %d", ln->client.age);
+            printf("\n\n");
+        }  
+    }
+
+    return;
+}
+
+void update(List* list, int id, Client client)
+{
+    for (ListNode* ln = list->first; ln != NULL; ln = ln->next)
+    {
+        if (ln->client.id == id) {
+            ln->client.id = id;
+            strcpy(ln->client.name, client.name);
+            ln->client.age = client.age;
+        }  
+    }
+}
+
+void toRemove(List* list, int id)
+{
+    ListNode* ant = NULL;
+    ListNode* actualElement = list->first;
+    
+    while (actualElement != NULL && actualElement->client.id != id)
+    {
+        ant = actualElement;
+        actualElement = actualElement->next;
+    }
+
+    if (ant == NULL) { 
+        list->first = actualElement->next;
+    } else {
+        ant->next = actualElement->next;
+    }
+
+    free(actualElement);
+}
+
+void saveToFile(List* list)
+{
+    FILE* fptr;
+
+    fptr = fopen(DATABASE, "w");
+
+    if (fptr == NULL) {
+        if (fptr == NULL) {
+            notFound("File");
+            exit(1);
+        }
+        exit(1);
+    }
+
+    for (ListNode* ln = list->first; ln != NULL; ln = ln->next)
+    {
+        printf("Id: %d ", ln->client.id);
+        printf("Nome: %s ", ln->client.name);
+        printf("Age: %d ", ln->client.age);
+        printf("Active: %d ", ln->client.active);
+        printf("\n");
+
+        if (ln->client.active == 1) {
+            fprintf(fptr, "%d %s %d %d \n",
+                ln->client.id, 
+                ln->client.name,
+                ln->client.age,
+                ln->client.active
+            );
+        }
+    }
+
+    fclose(fptr);
+
+    printWithLine("O registro foi armazenado no banco de dados!");
+}
+
+void loadFromFile(List* database)
+{
+    FILE* fptr;
+
+    fptr = fopen(DATABASE, "r");
+
+    if (fptr == NULL) {
+        notFound("File");
+        exit(1);
+    }
+
+    Client client;
+        
+    while ((fscanf(
+                fptr,
+                "%d %s %d %d\n",
+                &client.id, 
+                &client.name, 
+                &client.age, 
+                &client.active
+            )) != EOF)
+    {        
+        if (client.active == 1) {
+            insert(database, client);
+        }
+    }
+
+    fclose(fptr);
+}
--- main.c
+++ main.c
@@ -1,203 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-#include "helpers.h"
+#include "list/list.h"
 
 #define DATABASE "./database.txt"
-
-typedef struct list List;
-typedef struct client Client;
-typedef struct listNode ListNode;
-
-struct client
-{
-    int id;
-    int age;
-    char name[10];
-    int active;
-};
-
-struct list
-{
-    ListNode* first;
-    ListNode* last;
-    int totalElements;
-};
-
-struct listNode
-{
-    Client client;
-    ListNode* next;
-};
-
-
-List* create()
-{
-    List* list = (List*) malloc(sizeof(List));
-    list->first = NULL;
-    list->totalElements = 0;
-
-    return list;
-}
-
-void insert(List* list, Client client)
-{
-    ListNode *ant = NULL;
-    ListNode *first = list->first;
-    ListNode* node = (ListNode*) malloc(sizeof(ListNode));
-    
-
-    while (first != NULL)
-    {
-        ant = first;
-        first = first->next;
-    }
-    
-    client.active = 1;
-    client.id = list->totalElements;
-
-    node->client = client;
-
-    if (ant == NULL) {
-        node->next = list->first; //Seta o fim da lista, atribuindo null a última posição do ponteiro na primeira atribuição
-        list->first = node;
-    } else {
-        node->next = ant->next;
-        ant->next = node;
-    }
-    
-    list->totalElements ++;
-    
-    return;
-}
-
-void getAll(List* list)
-{
-    for (ListNode* ln = list->first; ln != NULL; ln = ln->next)
-    {
-        printf("Id: %d ", ln->client.id);
-        printf("Nome: %s ", ln->client.name);
-        printf("Idade: %d ", ln->client.age);
-        printf("\n");
-    }
-
-    return;
-}
-
-void get(List* list, int id)
-{
-    for (ListNode* ln = list->first; ln != NULL; ln = ln->next)
-    {
-        if (ln->client.id == id) {
-            printf("\n\n");
-            printf("Id: %d ", ln->client.id);
-            printf("Nome: %s ", ln->client.name);
-            printf("Idade: %d", ln->client.age);
-            printf("\n\n");
-        }  
-    }
-
-    return;
-}
-
-void update(List* list, int id, Client client)
-{
-    for (ListNode* ln = list->first; ln != NULL; ln = ln->next)
-    {
-        if (ln->client.id == id) {
-            ln->client.id = id;
-            strcpy(ln->client.name, client.name);
-            ln->client.age = client.age;
-        }  
-    }
-}
-
-void delete(List* list, int id)
-{
-    ListNode* ant = NULL;
-    ListNode* actualElement = list->first;
-    
-    while (actualElement != NULL && actualElement->client.id != id)
-    {
-        ant = actualElement;
-        actualElement = actualElement->next;
-    }
-
-    if (ant == NULL) { 
-        list->first = actualElement->next;
-    } else {
-        ant->next = actualElement->next;
-    }
-
-    free(actualElement);
-}
-
-void save(List* list)
-{
-
-    FILE *fptr;
-
-    fptr = fopen(DATABASE, "w");
-
-    if (fptr == NULL) {
-        if (fptr == NULL) {
-            notFound("File");
-            exit(1);
-        }
-        exit(1);
-    }
-
-    for (ListNode* ln = list->first; ln != NULL; ln = ln->next)
-    {
-        printf("Id: %d ", ln->client.id);
-        printf("Nome: %s ", ln->client.name);
-        printf("Age: %d ", ln->client.age);
-        printf("Active: %d ", ln->client.active);
-        printf("\n");
-
-        if (ln->client.active == 1) {
-            fprintf(fptr, "%d %s %d %d \n",
-                ln->client.id, 
-                ln->client.name,
-                ln->client.age,
-                ln->client.active
-            );
-        }
-    }
-
-    fclose(fptr);
-
-    printWithLine("O registro foi armazenado no banco de dados!");
-}
-
-void loadFile(List* database)
-{
-    FILE *fptr;
-
-    fptr = fopen(DATABASE, "r");
-
-    if (fptr == NULL) {
-        notFound("File");
-        exit(1);
-    }
-
-    Client client;
-        
-    while ((fscanf(
-                fptr,
-                "%d %s %d %d\n",
-                &client.id, 
-                &client.name, 
-                &client.age, 
-                &client.active
-            )) != EOF)
-    {        
-        if (client.active == 1) {
-            insert(database, client);
-        }
-    }
-
-    fclose(fptr);
-}
 
 void menu()
 {
@@ -236,10 +39,10 @@ void menu()
                 printf("Insira o Id do usuário a ser atualizado: ");
                 scanf("%d", &id);
 
-                if (id < 0 || id > database->totalElements) {
+                if (id < 0 || id > database->totalElements -1 ) {
                     notFound("Id");
 
-                    break;;
+                    break;
                 }
                 
                 printf("Insira o novo nome do usuário: ");
@@ -283,23 +86,28 @@ void menu()
                 printf("Insira o id do usuário a ser deletado: ");
                 scanf("%d", &id);
 
-                delete(database, id);
+                if (id > database->totalElements - 1) {
+                    notFound("User");
+                    break;
+                }
+
+                toRemove(database, id);
                 break;  
 
             case 6:
                 if (database->first == NULL) {
                     printWithLine("Não há registros para serem gravados no banco de dados");
                     
-                    break;;
+                    break;
                 }
 
-                save(database);
+                saveToFile(database);
                 break;
 
             case 7:
                 printWithLine("Carregando registros do banco de dados...");
 
-                loadFile(database);
+                loadFromFile(database);
                 
                 sleep(5);
                 printWithLine("Registros carregados em memória com sucesso!");
